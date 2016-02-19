06531d847b63516b1df906abe7cd7283694551f2
Anthony Olsen <design@joomlabamboo.com>
2016-02-19 16:34:32 +1100

    Package

--- src/zengridframework.php
+++ src/zengridframework.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @package     ##package##
+ * @subpackage  ##subpackage##
+ * @author      ##author##
+ * @copyright   ##copyright##
+ * @license     ##license##
+ * @version     ##version##
+ */
+ 
 // no direct access
 defined('_JEXEC') or die;
 
